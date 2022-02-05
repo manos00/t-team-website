3e318855e6e14f95fb0e2021a603500b9b8cfbc0
manos00 <manos.wagner@gmail.com>
2022-02-05 12:16:42 +0100

    basic structure of the website

---
+++ main.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title>T-Team - Startseite</title>
+</head>
+<body>
+    <div id="navbar">
+        <div class="navbaritem">
+            <a href="main.php">Startseite</a>
+        </div>
+        <div class="navbaritem">
+            <a href="">FAQ</a>
+        </div>
+        <div class="navbaritem">
+            <a href="">WRG</a>
+        </div>
+        <div class="navbaritem">
+            <a href="">Über uns</a>
+        </div>
+    </div>
+    <br>
+    <div id="welcome">
+        <h1>T-TEAM</h1>
+        <div class=text>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti sit inventore quae, expedita libero quia repellat molestias incidunt, magnam obcaecati esse numquam fugit necessitatibus, consequatur rem laudantium tempora accusantium quasi?
+            <!--fix this weird linebreak stuff-->
+            <br>
+            <br>
+            <button onclick="location.href='#formular'" type="button" class="btn">Zum Formular</button>
+        </div>
+    </div>
+    <br>
+    <div id="formular">
+        <h2>Formular</h2>
+        <?php ?>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit aut quo ipsa labore sequi ducimus possimus a! Ut, repudiandae explicabo? Placeat aliquid facilis magnam sequi error rerum nulla blanditiis qui.
+    </div>
+    <br>
+</body>
+<footer>
+    <div class="footerrow">
+        <div>Zurück nach oben</div>
+    </div>
+    <br>
+    <div class="footerrow">
+        <a>Impressum</a>
+        <a>Haftungsausschluss</a>
+        <a>Datenschutz</a>
+        <a>Über uns</a>
+    </div>
+</footer>
+</html>
